96a44f2c50f0dac15d2e6510df435d8f86b63b8a
Alberto Navarro <alnav@amazon.com>
2014-07-16 22:33:52 -0400

    Questions Render

--- wizard.js
+++ wizard.js
@@ -1,60 +1,83 @@
-;var wizard = wizard | {};
+var wizard = wizard || {};
 
 wizard.prototype = {
-	
-	xmlReader: function(){
+
+	wizard: this,
+
+	xmlReader: function() {
 		return 1;
 	},
+
+
 	/*
+	@description gather questions and trigger render for each questions.
+	@param questions, object with questions. JSON
+	*/
+	
+	wizardRenderer: function render( qs ) {
+	
+		var wizardHTML = document.createElement( "div" );
+		wizardHTML.setAttribute( 'class', 'wizard' );
+		
+		if ( qs.length ) {
+			qs.forEach(function(){
+				wizardHTML.appendChild(wizard.question(this));
+			})
+	
+		} else {
+			console.error( "Problems with questions json, no questions attached." );
+		}
+	},
+
+  /*
 	@description: Renders A question and its multiple answers.
 	@param question String with the question to ask | Can receive HTML code.
 	@param answers Object contains answers to display and value for the answer.
 			Answer; contain an aswer can be HTML.
 			Value; String contain the value or string to the answer.
-	
+
 	*/
-	questionRender: function(question,answers,qID){
-		
-		qID ? null : Math.random();
-		
-		var htmlQuestion = document.createElement("div");
-		htmlQuestion.setAttribute('id', "question"+qID);
-		htmlQuestion.setAttribute('class', 'question');
-		var htmlText = document.createElement("header");
+	questionRender: function q( question, answers, qID ) {
+
+		qID = qID ? null : Math.random();
+
+		var htmlQuestion = document.createElement( "div" );
+		htmlQuestion.setAttribute( 'id', "question" + qID );
+		htmlQuestion.setAttribute( 'class', 'question' );
+		var htmlText = document.createElement( "header" );
 		htmlText.innerHTML = question;
-		htmlQuestion.appendChild(htmlText);
-		
-		if (answers.length){
-			
-			var htmlAnswers  = document.createElement("div");
-			htmlAnswers.setAttribute('id', "answers"+qID);
-			htmlAnswers.setAttribute('class','answers');
-			
-			
-			answers.forEach(function(answers,iteration){
-						
-				var label = document.createElement("Label");
-				label.for = qID+ "an" + iteration;
+		htmlQuestion.appendChild( htmlText );
+
+		if ( answers.length ) {
+
+			var htmlAnswers = document.createElement( "div" );
+			htmlAnswers.setAttribute( 'id', "answers" + qID );
+			htmlAnswers.setAttribute( 'class', 'answers' );
+
+
+			answers.forEach( function( answer, iteration ) {
+
+				var label = document.createElement( "Label" );
+				label.for = (qID + "an" + iteration);
 				label.innerHTML = answer.html;
-				
-				var input = document.createElement("input");
-				input.setAttribute("type", "radio");
-				input.id = qID + 'an'+iteration;
+
+				var input = document.createElement( "input" );
+				input.setAttribute( "type", "radio" );
+				input.id = qID + 'an' + iteration;
 				input.value = answer.value;
-				
-				htmlAnswers.appendChild(label);
-				htmlAnswers.appendChild(input);
-				
-			});
-		
-		}else{ console.error("malformed question, no answers."); }
-	
-	
-		htmlQuestion.appendChild(htmlAnswers);
+
+				htmlAnswers.appendChild( label );
+				htmlAnswers.appendChild( input );
+
+			} );
+
+		} else {
+			console.error( "malformed question, no answers." );
+		}
+
+
+		htmlQuestion.appendChild( this.htmlAnswers );
 		return htmlQuestion;
 	}
-		
-	
-	
-	
+
 };
